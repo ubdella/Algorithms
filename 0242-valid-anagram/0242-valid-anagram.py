bcada9cf31f47fd692ba1b62f4bce7410f74f02c
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2023-07-27 21:18:22 +0530

    Time: 54 ms (92.75%), Space: 16.9 MB (65.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,15 +2,16 @@ class Solution:
     def isAnagram( self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        ls = list(s)
-        lt = list(t)
-        while(len(ls)!=0):
-            item = ls.pop(0)
-            if(item in lt):
-                lt.remove(item)
-            else:
-                return False
-        return ls==lt
+        smap = {}
+        tmap = {}
+        
+        for i in range(len(s)):
+            smap[s[i]] = smap.get(s[i],0) + 1
+            tmap[t[i]] = tmap.get(t[i],0) + 1
+
+        return smap==tmap
+
+
 
                 
         
