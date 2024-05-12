9b70a4002585eee2dd6ed94cdb9ad5f7e17f6cf4
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-05-12 06:45:57 -0400

    Time: 43 ms (20.13%), Space: 16.7 MB (11.38%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,42 +1,22 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        len_a = len(a)
-        len_b = len(b)
-        
-        if len_a > len_b:
-            for i in range(len_a - len_b):
-                b = '0' + b
-        else:
-            for i in range(len_b - len_a):
-                a = '0' + a
-        if a == b == '0':
-            return '0'
         carry = 0
         result = ''
+        
+        if len(a) > len(b):
+            for i in range(len(a) - len(b)):
+                b = '0' + b
+        else:
+            for i in range(len(b) - len(a)):
+                a = '0' + a
+        
         for i in range(len(a)-1, -1, -1):
-            s = int(a[i]) + int(b[i]) + carry
-            if i == 0:
-                if s == 0:
-                    return result
-                elif s == 1:
-                    return '1' + result
-                elif s == 2:
-                    return '10' + result
-                elif s == 3:
-                    return '11' + result
-                
-                    
-            if s == 2:
-                carry = 1
-                result = '0' + result
-            elif s == 3:
-                carry = 1
-                result = '1' + result
-            elif s == 1:
-                carry = 0
-                result = '1' + result
-            else:
-                carry = 0
-                result = '0' + result
+            s = carry + int(a[i]) + int(b[i])
+            result = str(s%2) + result
+            carry = s//2
+        if carry:
+            result = '1' + result
+        
+        return result
             
         
