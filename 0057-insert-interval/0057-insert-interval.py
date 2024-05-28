6948dbdfb1385a54cedb58e5fc84d123c19a01da
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-05-28 04:50:57 -0400

    Time: 67 ms (91.72%), Space: 19.9 MB (76.41%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,7 +1,6 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
-        
         for i in range(len(intervals)):
             if newInterval[1]<intervals[i][0]:
                 res.append(newInterval)
@@ -10,6 +9,5 @@ class Solution:
                 res.append(intervals[i])
             else:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-                
         res.append(newInterval)
         return res
