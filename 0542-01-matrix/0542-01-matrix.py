59b7aee9080e8c253e4177fe2fc7e971921ccf80
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-05-29 07:05:42 -0400

    Time: 404 ms (88.77%), Space: 19.3 MB (85.48%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,23 +1,22 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        # BFS
-        # Time O(R * C)     Space O(R * C)
-        ROW, COL = len(mat), len(mat[0])
-        q = deque()
-        for r in range(ROW):
-            for c in range(COL):
-                if mat[r][c] == 0:
-                    q.append((r, c))
+        m, n, q = len(mat), len(mat[0]), deque()
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    q.append((i, j))
                 else:
-                    mat[r][c] = -1
-                    
+                    mat[i][j] = '#'
+        
         while q:
-            r, c = q.popleft()
-            for t_r, t_c in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                new_r = r + t_r
-                new_c = c + t_c
-                if new_r < 0 or new_r >= ROW or new_c < 0 or new_c >= COL or mat[new_r][new_c] != -1:
+            i, j  = q.popleft()
+            
+            for di, dj in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                r = i + di
+                c = j + dj
+                if r < 0 or r >= m or c < 0 or c >= n or mat[r][c]!='#':
                     continue
-                q.append((new_r, new_c))
-                mat[new_r][new_c] = mat[r][c] + 1
+                q.append((r, c))
+                mat[r][c] = mat[i][j] + 1
         return mat
+        
