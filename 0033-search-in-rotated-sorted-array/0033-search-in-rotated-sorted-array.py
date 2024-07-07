0024e1831a3ab1b81a0251347a3851a0a64c0f4e
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-07-07 03:04:04 -0400

    Time: 45 ms (45.51%), Space: 16.8 MB (73.09%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,19 +3,14 @@ class Solution:
         l, r = 0, len(nums)-1
         while l<=r:
             m = (l+r)//2
-            if nums[m]==target: return m
-            
-            if nums[m]>=nums[l]:
-                #We're in the left sorted subset
-                if target>nums[m] or target<nums[l]:
-                    l = m+1
-                else:
-                    r = m-1
+            if nums[m] == target: return m
+            #We're in the left sorted part:
+            if nums[m]>nums[r]:
+                if target<nums[l] or target>nums[m]: l = m+1
+                else: r = m-1
+                    
+            #We're in the right sorted part
             else:
-                #We're in the right sorted subset
-                if target<nums[m] or target>nums[r]:
-                    r = m-1
-                else:
-                    l = m+1
-
+                if target>nums[r] or target<nums[m]: r = m-1
+                else: l = m+1
         return -1
