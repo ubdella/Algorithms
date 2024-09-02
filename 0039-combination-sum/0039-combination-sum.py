c6a9fc5950170cc3f7aea3c55c403222ed0ce83b
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-02 01:06:45 -0400

    Time: 57 ms (36.33%), Space: 16.7 MB (44.36%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,18 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def dfs(yet, i):
-            current_sum = sum(yet)
-            if current_sum == target:
-                res.append(yet.copy())  # Append a copy of the list to avoid mutation issues
+        def dfs(yet, sumYet, i):
+            if sumYet == target: 
+                res.append(yet.copy())
                 return
-            if current_sum > target:
+            if i >= len(candidates) or sumYet > target:
                 return
             
-            for j in range(i, len(candidates)):
-                yet.append(candidates[j])
-                dfs(yet, j)  # Continue with the current candidate
-                yet.pop()  # Backtrack by removing the last added element
+            dfs(yet.copy(), sumYet, i + 1)
+            
+            yet.append(candidates[i])
+            
+            dfs(yet.copy(), sumYet + candidates[i], i)
+                
                 
         res = []
-        dfs([], 0)
+        dfs([], 0, 0)
         return res
