c0c1601778b1bada83ee0d989225ceb5f917be01
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-05 03:09:15 -0400

    Time: 208 ms (69.81%), Space: 18.4 MB (21.62%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,30 +1,25 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights or not heights[0]:
+            return []
+        
         m, n = len(heights), len(heights[0])
-        def dfs(i, j, h):
-            if i < 0 or i >= m or j < 0 or j >= n or heights[i][j] > h or (i, j) in visited:
-                return [0, 0]
-            visited.add((i, j))
-            curHeight = heights[i][j]
-            pacific, atlantic = 0, 0
-            if i == 0 or j == 0:
-                pacific = 1
-            if i == m-1 or j == n-1:
-                atlantic = 1
-            
-            for di, dj in (1, 0), (-1, 0), (0, 1), (0, -1):
-                p, a = dfs(i + di, j + dj, curHeight)
-                pacific, atlantic = pacific or p, atlantic or a
-    
-            return [pacific, atlantic]
-        res = []
-
+        pacific = [[False] * n for _ in range(m)]
+        atlantic = [[False] * n for _ in range(m)]
+        
+        def dfs(i, j, reachable):
+            reachable[i][j] = True
+            for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < m and 0 <= nj < n and not reachable[ni][nj] and heights[ni][nj] >= heights[i][j]:
+                    dfs(ni, nj, reachable)
+        
         for i in range(m):
-            for j in range(n):
-                visited = set()
-                both = dfs(i, j, heights[i][j])
-                print(both)
-                if both == [1, 1]:
-                    res.append([i, j])
-        return res
-            
+            dfs(i, 0, pacific)
+            dfs(i, n-1, atlantic)
+        
+        for j in range(n):
+            dfs(0, j, pacific)
+            dfs(m-1, j, atlantic)
+        
+        return [[i, j] for i in range(m) for j in range(n) if pacific[i][j] and atlantic[i][j]]
