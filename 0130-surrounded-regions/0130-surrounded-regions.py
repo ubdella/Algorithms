c0ab5fb96caba1ae47b2c8ea1b26a933b026def1
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-06 14:19:31 -0400

    Time: 112 ms (90.20%), Space: 17.9 MB (56.92%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -4,24 +4,29 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         def dfs(i, j):
-            if i == m or j == n or i < 0 or j < 0 or (i, j) in visited or board[i][j] == "X": 
-                return True
-            if i == m - 1 or i == 0 or j == 0 or j == n - 1: 
-                return False
-            visited.add((i, j))
-            visitedOuter.add((i, j))
-            surrounded = True
-            for di, dj in (1, 0), (-1, 0), (0, 1), (0, -1):
-                if not dfs(i + di, j + dj):
-                    surrounded = False
-            return surrounded
+            if i == m or j == n or i < 0 or j < 0 or board[i][j] != "O": 
+                return 
+            board[i][j] = "T"
+            for di, dj in (1, 0), (-1, 0), (0, -1), (0, 1):
+                dfs(i + di, j + dj)
         m, n = len(board), len(board[0])
-        visitedOuter = set()
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n - 1)
+        
+        for i in range(n):
+            dfs(0, i)
+            dfs(m -1, i)
+            
         for i in range(m):
             for j in range(n):
-                visited = set()
-                if (i, j) not in visitedOuter and board[i][j] == "O" and dfs(i, j):
-                    for r, c in visited:
-                        board[r][c] = "X"
+                if board[i][j] == "T":
+                    board[i][j] = "O"
+                else:
+                    board[i][j] = "X"
+                
+                
+            
+            
         
         
