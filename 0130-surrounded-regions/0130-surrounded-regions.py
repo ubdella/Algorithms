f0279da7d15f9253c7c84bcd371c0d60d7844c12
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-06 12:56:10 -0400

    Time: 177 ms (8.82%), Space: 35.8 MB (5.07%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -9,16 +9,18 @@ class Solution:
             if i == m - 1 or i == 0 or j == 0 or j == n - 1: 
                 return False
             visited.add((i, j))
+            visitedOuter.add((i, j))
             surrounded = True
             for di, dj in (1, 0), (-1, 0), (0, 1), (0, -1):
                 if not dfs(i + di, j + dj):
                     surrounded = False
             return surrounded
         m, n = len(board), len(board[0])
+        visitedOuter = set()
         for i in range(m):
             for j in range(n):
                 visited = set()
-                if board[i][j] == "O" and dfs(i, j):
+                if (i, j) not in visitedOuter and board[i][j] == "O" and dfs(i, j):
                     for r, c in visited:
                         board[r][c] = "X"
         
