d78f86ec53a9ef52b26c34f2a99e9241ce825b84
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-06 21:10:36 -0400

    Time: 73 ms (8.12%), Space: 17.7 MB (7.61%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,22 +1,18 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        def connected(source, target):
+            if source in visited:
+                return False
+            if source == target:
+                return True
+            visited.add(source)
+            return any(connected(neighbour, target) for neighbour in graph[source])
+            
         graph = {}
         
-        def dfs(source, target):
-            if source not in seen:
-                seen.add(source)
-                if source == target:
-                    return True
-                for nei in graph[source]:
-                    if dfs(nei, target):
-                        return True
-            return False
-        
         for u, v in edges:
-            seen = set()
-            if u in graph and v in graph and dfs(u, v):
+            visited = set()
+            if u in graph and v in graph and connected(u, v):
                 return [u, v]
             graph.setdefault(u, []).append(v)
             graph.setdefault(v, []).append(u)
-        
-        return []
