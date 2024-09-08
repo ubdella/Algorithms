cd0a69a6a0576af8a6946c06d5ba82d5d183deed
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-07 20:14:05 -0400

    Time: 39 ms (21.92%), Space: 16.6 MB (14.63%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,7 @@
 class Solution:
-    def __init__(self):
-        self.cache = {}
     def climbStairs(self, n: int) -> int:
-        if n in self.cache: return self.cache[n]
-        if not n: return 1
-        if n < 0: return 0
-        self.cache[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
-        return self.cache[n]
+        dp = [0, 1, 2]
+        for i in range(3, n + 1):
+            dp.append(dp[i - 1] + dp[i - 2])
+        return dp[n]
         
