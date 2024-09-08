9b5fa3dc047b230eef750a879fbd74c7cb23f890
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-07 20:04:17 -0400

    Time: 37 ms (37.85%), Space: 16.5 MB (14.63%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,7 +1,10 @@
 class Solution:
-    @cache
+    def __init__(self):
+        self.cache = {}
     def climbStairs(self, n: int) -> int:
+        if n in self.cache: return self.cache[n]
         if not n: return 1
         if n < 0: return 0
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        self.cache[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.cache[n]
         
