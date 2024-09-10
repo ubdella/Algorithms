8110d0503b78762b87b86bc19d64d075334e9478
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-10 04:47:30 -0400

    Time: 32 ms (80.35%), Space: 16.6 MB (52.67%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -7,15 +7,10 @@ class Solution:
         dp = [0] * (n + 1)
         dp[0] = 1
         dp[1] = 1
-        
         for i in range(2, n + 1):
-            # Single digit decode
-            if s[i-1] != '0':
-                dp[i] += dp[i-1]
-            
-            # Two digit decode
-            two_digit = int(s[i-2:i])
-            if 10 <= two_digit <= 26:
-                dp[i] += dp[i-2]
-        
+            if s[i - 1] != '0':
+                dp[i] += dp[i - 1]
+                
+            if 10 <= int(s[i - 2 : i]) <= 26:
+                dp[i] += dp[i  - 2]
         return dp[n]
