8c2605ae818e2ae6ff9a159fa2f301c966022302
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-12 23:50:11 -0400

    Time: 46 ms (9.37%), Space: 16.4 MB (93.32%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,24 +1,33 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        
-        # Base case: empty string is valid
-        for i in range(n):
-            if s[i] == '*':
-                dp[i][i] = True
-            if i < n - 1 and (s[i] == '(' or s[i] == '*') and (s[i+1] == ')' or s[i+1] == '*'):
-                dp[i][i+1] = True
-        
-        # Fill the dp table
-        for length in range(3, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
+        # First pass: left to right
+        open_count = star_count = 0
+        for char in s:
+            if char == '(':
+                open_count += 1
+            elif char == '*':
+                star_count += 1
+            else:  # char == ')'
+                if open_count > 0:
+                    open_count -= 1
+                elif star_count > 0:
+                    star_count -= 1
+                else:
+                    return False
                 
-                if (s[i] == '(' or s[i] == '*') and (s[j] == ')' or s[j] == '*'):
-                    dp[i][j] = dp[i+1][j-1]
-                
-                for k in range(i, j):
-                    dp[i][j] |= dp[i][k] and dp[k+1][j]
-        
-        return dp[0][n-1]
+            closeCount = starCount = 0
+            
+            for i in range(len(s) - 1, -1, -1):
+                c = s[i]
+                if c == ')':
+                    closeCount += 1
+                elif c == '*':
+                    starCount += 1
+                else:
+                    if closeCount > 0:
+                        closeCount -= 1
+                    elif starCount > 0:
+                        starCount -= 1
+                    else:
+                        return False
+        return True
