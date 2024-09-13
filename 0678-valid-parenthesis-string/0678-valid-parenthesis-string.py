c3faf59cc403f86344f9fd8bd49c07db76aee097
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-12 23:43:54 -0400

    Time: 249 ms (5.20%), Space: 16.5 MB (72.74%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,22 +1,24 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        leftMin, leftMax = 0, 0
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
         
-        for char in s:
-            if char == '(':
-                leftMin += 1
-                leftMax += 1
-            elif char == ')':
-                leftMin -= 1
-                leftMax -= 1
-            else:  # char == '*'
-                leftMin -= 1
-                leftMax += 1
-            
-            if leftMax < 0:
-                return False
-            
-            if leftMin < 0:
-                leftMin = 0
+        # Base case: empty string is valid
+        for i in range(n):
+            if s[i] == '*':
+                dp[i][i] = True
+            if i < n - 1 and (s[i] == '(' or s[i] == '*') and (s[i+1] == ')' or s[i+1] == '*'):
+                dp[i][i+1] = True
         
-        return leftMin == 0
+        # Fill the dp table
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                
+                if (s[i] == '(' or s[i] == '*') and (s[j] == ')' or s[j] == '*'):
+                    dp[i][j] = dp[i+1][j-1]
+                
+                for k in range(i, j):
+                    dp[i][j] |= dp[i][k] and dp[k+1][j]
+        
+        return dp[0][n-1]
