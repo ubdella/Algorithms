05c91a1f1cb456fe20b78444bc4d7adf325695d0
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-12 23:01:42 -0400

    Time: 47 ms (22.00%), Space: 16.4 MB (71.60%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,26 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        
+        def getPos(c):
+            return ord(c) - ord('a')
+        
+        endPos = [0] * 26
+        for i in range(len(s)):
+            endPos[getPos(s[i])] = i
+        
+        res = []
+        
+        i = j = 0
+        splitPos = endPos[getPos(s[0])]
+        while j < len(s):
+            if j == splitPos:
+                res.append(j - i + 1)
+                i = j + 1
+                if i < len(s):
+                    splitPos = endPos[getPos(s[i])]
+            if endPos[getPos(s[j])] > splitPos:
+                splitPos = endPos[getPos(s[j])]
+                
+            j += 1
+            
+        return res
