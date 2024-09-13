f81658a6435cddbf3e350f93b258ddef5928b9a1
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-12 23:08:52 -0400

    Time: 41 ms (57.94%), Space: 16.5 MB (30.37%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,20 +1,21 @@
 class Solution:
-    def partitionLabels(self, s: str) -> List[int]:
-        def getPos(c):
-            return ord(c) - ord('a')
-        endPos = [0] * 26
-        for i in range(len(s)):
-            endPos[getPos(s[i])] = i
+    def partitionLabels(self, S: str) -> List[int]:
+        count = {}
         res = []
-        i = j = 0
-        splitPos = endPos[getPos(s[0])]
-        while j < len(s):
-            if j == splitPos:
-                res.append(j - i + 1)
-                i = j + 1
-                if i < len(s):
-                    splitPos = endPos[getPos(s[i])]
-            if endPos[getPos(s[j])] > splitPos:
-                splitPos = endPos[getPos(s[j])]   
-            j += 1 
+        i, length = 0, len(S)
+        for j in range(length):
+            c = S[j]
+            count[c] = j
+
+        curLen = 0
+        goal = 0
+        while i < length:
+            c = S[i]
+            goal = max(goal, count[c])
+            curLen += 1
+
+            if goal == i:
+                res.append(curLen)
+                curLen = 0
+            i += 1
         return res
