32a9aad0790abb2fba947cd18e6057738877da38
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-14 01:25:08 -0400

    Time: 105 ms (19.51%), Space: 23.3 MB (5.41%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        def getOnes(bits):
+            ones = 0
+            while bits:
+                ones += bits & 1
+                bits = bits >> 1
+            return ones
+        res = []
+        for digit in range(n + 1):
+            res.append(getOnes(digit))
+        return res
