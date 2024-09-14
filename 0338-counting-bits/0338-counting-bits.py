ba61d9a46f809a1de6d535e7622992b0c1444e41
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-14 01:31:55 -0400

    Time: 115 ms (13.46%), Space: 23.2 MB (44.01%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -4,7 +4,7 @@ class Solution:
             ones = 0
             while bits:
                 if bits % 2 != 0:
-                    ones += bits & 1
+                    ones += 1
                 bits = bits >> 1
             return ones
         res = []
