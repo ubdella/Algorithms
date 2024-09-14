359c52c8f9306c45d2542f5ecdb1e5838d0eff6e
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-14 02:39:09 -0400

    Time: 35 ms (66.97%), Space: 16.5 MB (45.25%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,9 +1,13 @@
 class Solution:
     def reverseBits(self, n):
-        # Convert to binary string, remove '0b' prefix, zero-pad to 32 bits
-        binary_str = format(n, '032b')
-        # Reverse the string
-        reversed_str = binary_str[::-1]
-        # Convert back to integer
-        return int(reversed_str, 2)
+        res = 0
+
+        for i in range(32):
+
+            res = res << 1
+            bit = n%2
+            res += bit
+            n = n >> 1
+
+        return res
         
