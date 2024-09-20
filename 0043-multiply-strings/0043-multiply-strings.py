0d6625eb6d36eedc8616501a3b7d88d4aaaa7aa9
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-20 03:39:04 -0400

    Time: 100 ms (27.33%), Space: 16.6 MB (20.92%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -4,22 +4,14 @@ class Solution:
             return "0"
 
         # Initialize the result array with zeros
-        m, n = len(num1), len(num2)
-        result = [0] * (m + n)
-
-        # Perform multiplication digit by digit
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
+        res = [0] * (len(num1) + len(num2)) 
+        
+        for i in range(len(num1) - 1, -1, -1):
+            for j in range(len(num2) -1, -1, -1):
                 mul = int(num1[i]) * int(num2[j])
-                p1, p2 = i + j, i + j + 1
-                total = mul + result[p2]
-
-                result[p2] = total % 10
-                result[p1] += total // 10
-
-        # Convert the result to a string, removing leading zeros
-        start = 0
-        while start < len(result) - 1 and result[start] == 0:
-            start += 1
-
-        return ''.join(map(str, result[start:]))
+                pos, nextPos = i + j + 1, i + j
+                total = mul + res[pos]
+                res[pos] = total % 10
+                res[nextPos] += total // 10
+        res = map(str, res)
+        return "".join(res).lstrip("0")
