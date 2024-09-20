82c48b6434f958c9fcd42e42f6ba77257a44153f
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-20 04:30:33 -0400

    Time: 243 ms (73.07%), Space: 18.5 MB (47.30%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -3,19 +3,19 @@ from collections import defaultdict
 class DetectSquares:
     def __init__(self):
         self.point_counts = defaultdict(int)
-        self.points = []
+        # self.points = []
 
     def add(self, point):
         x, y = point
         self.point_counts[(x, y)] += 1
-        self.points.append((x, y))
+        # self.points.append((x, y))
 
     def count(self, point):
         qx, qy = point
         count = 0
 
-        for x, y in self.points:
+        for x, y in list(self.point_counts.keys()):
             if abs(qx - x) == abs(qy - y) and qx != x and qy != y:
-                count += self.point_counts[(x, qy)] * self.point_counts[(qx, y)]
+                count += self.point_counts[(x, y)] * self.point_counts[(x, qy)] * self.point_counts[(qx, y)]
 
         return count
