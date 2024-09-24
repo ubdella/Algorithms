f12caa02e9b4c33ef71c7cd2b0b7fd4ea84610b2
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-23 21:31:22 -0400

    Time: 2063 ms (5.34%), Space: 25.4 MB (27.68%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
@@ -1,14 +1,24 @@
 class Solution:
 
     def longestSubstring(self, s: str, k: int) -> int:
-        if not s or k > len(s):
-            return 0
+        def count_valid_chars(string: str, k: int) -> int:
+            char_count = {}
+            for char in string:
+                char_count[char] = char_count.get(char, 0) + 1
+            return sum(1 for count in char_count.values() if count >= k)
 
-        char_count = Counter(s)
+        def divide_and_conquer(start: int, end: int) -> int:
+            if end - start < k:
+                return 0
 
-        for char in set(s):
-            if char_count[char] < k:
-                return max(self.longestSubstring(substr, k) for substr in s.split(char))
+            char_count = {}
+            for i in range(start, end):
+                char_count[s[i]] = char_count.get(s[i], 0) + 1
 
-        return len(s)
-        
+            for mid in range(start, end):
+                if char_count[s[mid]] < k:
+                    return max(divide_and_conquer(start, mid), divide_and_conquer(mid + 1, end))
+
+            return end - start
+
+        return divide_and_conquer(0, len(s))    
