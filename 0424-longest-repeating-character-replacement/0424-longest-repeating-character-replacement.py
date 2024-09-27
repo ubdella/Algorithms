daf17d81f03ddfe745a62bad865190efe4b0cbbf
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-09-27 00:24:45 -0400

    Time: 109 ms (42.00%), Space: 16.7 MB (6.07%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,20 +1,26 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        i = longest = 0
-        counts = {}
-        for j in range(len(s)):
-            counts[s[j]] = 1 + counts.get(s[j], 0)
-            
-            if j - i + 1 - max(counts.values()) > k:
-                counts[s[i]] -= 1
-                i += 1
-                continue
-            
-            longest = max(longest, j - i + 1)
-        
-        
-                
-        return longest
-                
+        #initialize frequency for each character with 0
+        #initialize a left, intialize a result
+        #for traversing right pointer until the end of the string
+        #	update the counts
+        #	go through the counts to find the highest frequency element
+        #	if window length - highest frequency > k
+        #		left += 1
+        #	take the max of result and windowlength
+
+        #“AAAABBCCC” k = 2
+        #freq = {A: 3, B:2, C:2}
+        freq = [0] * 26
+        left = result = 0
+        for right in range(len(s)):
+            freq[ord(s[right]) - ord('A')] += 1
+            if right - left + 1 - max(freq) > k:
+
+                freq[ord(s[left]) - ord('A')] -= 1
+                left += 1
+            result = max(result, right - left + 1)
+        return result
+
             
                 
