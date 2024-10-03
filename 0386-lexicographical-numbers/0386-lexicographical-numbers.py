12970ae35b4965f605afca1608ffc336ff869209
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-10-02 21:11:16 -0400

    Time: 70 ms (70.67%), Space: 22.6 MB (48.05%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -2,13 +2,13 @@ class Solution:
 	def lexicalOrder(self, n):
 		result = []
 		current = 1
-		for i in range(n):
-			result.append(current)            
+		result.append(current)
+		for i in range(n - 1):
 			if current * 10 <= n:
 				current = current * 10
 			else:
 				while current % 10 == 9 or current + 1 > n:
 					current = current // 10
 				current += 1
-
+			result.append(current)
 		return result
