8dc281f4e964b48945f827b77f0c11b46f28140a
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-10-03 19:29:58 -0400

    Time: 53 ms (49.28%), Space: 16.7 MB (63.42%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,31 +1,20 @@
 class Solution:
-    def convert(self, s, numRows):
+    def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
-        # PAYPALISHIRING
-        dire = 'down'
-        i,j = 0,0
-        grid = [['']*len(s) for _ in range(numRows)]
-        for sIndex in range(len(s)):
-            
-            grid[i][j] = s[sIndex]
-            if dire == 'down':
-                if i == numRows - 1:
-                    dire = 'up'
-                    i -= 1
-                    j += 1
-                else:
-                    i += 1
-            else:
-                if i == 0:
-                    dire = 'down'
-                    i += 1
-                else:
-                    i -= 1
-                    j += 1
-        result = ''
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] != '':
-                    result += grid[i][j]
-        return result
+
+        rows = [''] * numRows
+        current_row = 0
+        step = 1
+
+        for char in s:
+            rows[current_row] += char
+
+            if current_row == 0:
+                step = 1
+            elif current_row == numRows - 1:
+                step = -1
+
+            current_row += step
+
+        return ''.join(rows)
