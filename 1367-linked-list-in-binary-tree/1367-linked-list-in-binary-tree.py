dbd9a16955ff34dd300d630e2dfc14dea042b871
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-10-04 21:59:32 -0400

    Time: 68 ms (57.40%), Space: 17.3 MB (63.10%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
@@ -10,30 +10,25 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSubPath(self, head: ListNode, root: TreeNode) -> bool:
-        def dfs(head: ListNode, root: TreeNode) -> bool:
-            # If we've reached the end of the linked list, we've found a match
-            if not head:
+
+    def dfsHelper(self, root, head):
+        if head == None:return True
+        if root == None:return False
+        if root.val != head.val:return False
+        return self.dfsHelper(root.left, head.next) or self.dfsHelper(root.right, head.next) 
+
+    def dfs(self, root, head):
+        if root == None:
+            return False
+        if root.val == head.val:
+            if self.dfsHelper(root.left, head.next):
                 return True
-            
-            # If we've reached the end of the tree path but not the end of the list, no match
-            if not root:
-                return False
-            
-            # If the current values match, continue checking
-            if root.val == head.val:
-                return dfs(head.next, root.left) or dfs(head.next, root.right)
-            
-            # If values don't match, return False
-            return False
-
-        # Base case: if the tree is empty, return False
-        if not root:
-            return False
-
-        # Check if the linked list starts from the current node
-        if dfs(head, root):
+            if self.dfsHelper(root.right, head.next):
+                return True
+        if self.dfs(root.left, head):
             return True
-
-        # If not, recursively check left and right subtrees
-        return self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
+        if self.dfs(root.right, head):
+            return True
+        return False
+    def isSubPath(self, head, root):
+        return self.dfs(root, head)
