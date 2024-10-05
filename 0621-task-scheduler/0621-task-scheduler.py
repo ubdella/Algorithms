09723a686677277412e40450497a76f8d9841462
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-10-05 05:41:06 -0400

    Time: 398 ms (76.10%), Space: 17.1 MB (38.88%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,24 @@
+from collections import Counter, deque
+import heapq
+
+class Solution:
+    def leastInterval(self, tasks, n):
+        counts = Counter(tasks)
+        ready = [-count for count in counts.values()]
+        heapq.heapify(ready)
+        coolingDown = deque()
+        time = 0
+        
+        while coolingDown or ready:
+            time += 1
+            if ready:
+                cnt = 1 + heapq.heappop(ready)
+                if cnt:
+                    coolingDown.append((cnt, time + n))
+            else:
+                time = coolingDown[0][1]
+            
+            if coolingDown and coolingDown[0][1] == time:
+                heapq.heappush(ready, coolingDown.popleft()[0])
+                
+        return time
