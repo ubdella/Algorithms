d481dfc0df515f5dcaac41db8ffebf25e04c2f3f
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-10-05 21:12:39 -0400

    Time: 41 ms (34.38%), Space: 16.8 MB (45.24%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -1,49 +1,25 @@
-from typing import List
-import operator
-
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
-        def evaluate(nums, ops):
-            stack = [nums[0]]
-            for i, op in enumerate(ops):
-                for num in nums[i+1:]:
-                    if op == '+':
-                        stack.append(stack.pop() + num)
-                    elif op == '-':
-                        stack.append(stack.pop() - num)
-                    elif op == '*':
-                        stack.append(stack.pop() * num)
-                    stack, num = num, stack.pop()
-                stack.append(num)
-            return stack
-
-        def generate_all_ways(nums, ops):
-            if not ops:
-                return nums
+        def compute(left, right, op):
             results = []
-            for i in range(len(ops)):
-                left = generate_all_ways(nums[:i+1], ops[:i])
-                right = generate_all_ways(nums[i+1:], ops[i+1:])
-                for l in left:
-                    for r in right:
-                        if ops[i] == '+':
-                            results.append(l + r)
-                        elif ops[i] == '-':
-                            results.append(l - r)
-                        elif ops[i] == '*':
-                            results.append(l * r)
+            for l in left:
+                for r in right:
+                    if op == '+':
+                        results.append(l + r)
+                    elif op == '-':
+                        results.append(l - r)
+                    elif op == '*':
+                        results.append(l * r)
             return results
 
-        nums = []
-        ops = []
-        num = 0
-        for char in expression:
-            if char.isdigit():
-                num = num * 10 + int(char)
-            else:
-                nums.append(num)
-                num = 0
-                ops.append(char)
-        nums.append(num)
+        if expression.isdigit():
+            return [int(expression)]
 
-        return generate_all_ways(nums, ops)
+        results = []
+        for i, char in enumerate(expression):
+            if char in ['+', '-', '*']:
+                left = self.diffWaysToCompute(expression[:i])
+                right = self.diffWaysToCompute(expression[i+1:])
+                results.extend(compute(left, right, char))
+
+        return results
