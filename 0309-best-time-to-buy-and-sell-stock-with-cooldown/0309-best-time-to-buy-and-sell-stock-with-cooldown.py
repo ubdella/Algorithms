8d1512a92e657be9722996314bad00f0e9b2daa9
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-10-07 18:39:19 -0400

    Time: 105 ms (9.16%), Space: 37.9 MB (6.06%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        @cache
+        def dp(i, j):
+            if j >= len(prices) or j >= len(prices):
+                return 0
+            if prices[i] > prices[j]:
+                return dp(j, j + 1)
+            return max(prices[j] - prices[i] + dp(j + 2, j + 3), dp(i, j + 1), dp(i + 1, j + 1))
+        return dp(0, 1)
