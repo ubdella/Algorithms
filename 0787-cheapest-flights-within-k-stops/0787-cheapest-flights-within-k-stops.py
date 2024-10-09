9d668b4496ca12cc04655515391e32cefa19db18
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-10-09 03:46:10 -0400

    Time: 203 ms (11.87%), Space: 17.6 MB (94.30%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,27 +1,15 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        adj = [[] for _ in range(n)]  # [[[cost, destination]]]
-        for source, destination, price in flights:
-            adj[source].append([price, destination])
-        
-        minHeap = [(0, src, k + 1)]  # [(cost, node, remaining stops)]
-        
-        # Use a dictionary to keep track of the minimum cost and remaining stops for each node
-        best = {src: [0, k + 1]}  # {node: [min_cost, max_remaining_stops]}
-        
-        while minHeap:
-            cost, node, stops = heapq.heappop(minHeap)
-            
-            if node == dst:
-                return cost
-            
-            if stops > 0:
-                for nei_cost, nei_node in adj[node]:
-                    new_cost = cost + nei_cost
-                    new_stops = stops - 1
-                    
-                    if nei_node not in best or new_cost < best[nei_node][0] or new_stops > best[nei_node][1]:
-                        heapq.heappush(minHeap, (new_cost, nei_node, new_stops))
-                        best[nei_node] = [new_cost, new_stops]
-        
-        return -1
+        prices = [float("inf")] * n
+        prices[src] = 0
+
+        for i in range(k + 1):
+            tmpPrices = prices.copy()
+
+            for s, d, p in flights:  # s=source, d=dest, p=price
+                if prices[s] == float("inf"):
+                    continue
+                if prices[s] + p < tmpPrices[d]:
+                    tmpPrices[d] = prices[s] + p
+            prices = tmpPrices
+        return -1 if prices[dst] == float("inf") else prices[dst]
