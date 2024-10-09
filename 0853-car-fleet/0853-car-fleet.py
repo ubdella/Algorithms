b73fd7a9b04559af437221956ac180765ee1728e
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-10-09 19:04:38 -0400

    Time: 677 ms (64.44%), Space: 38.3 MB (62.98%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,16 +1,10 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        n = len(position)
-        timeToFinish = [0]*n
-        zipped = list(zip(position, speed))
-        zipped.sort(key = lambda x: -x[0])
-        position, speed = zip(*zipped)
-
-        nextSlowCarTime = float('-inf')
-        res = 0
-        for i, pos in enumerate(position):
-            timeToFinish = (target - pos)/speed[i]
-            if timeToFinish > nextSlowCarTime:
-                res += 1
-                nextSlowCarTime = timeToFinish
-        return res
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
