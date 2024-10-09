f1605cda7a588c96f17840160195e5ac5f4017e6
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-10-09 19:04:18 -0400

    Time: 672 ms (69.61%), Space: 41.6 MB (7.44%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -5,10 +5,7 @@ class Solution:
         zipped = list(zip(position, speed))
         zipped.sort(key = lambda x: -x[0])
         position, speed = zip(*zipped)
-        #10 8 5 3 0
-        #2 4 1 3 1
-        
-        
+
         nextSlowCarTime = float('-inf')
         res = 0
         for i, pos in enumerate(position):
