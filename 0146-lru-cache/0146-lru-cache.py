76c90d1d771c01e010a08e1449c89bb29e8576e6
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-10-10 06:00:55 -0400

    Time: 590 ms (36.25%), Space: 78.4 MB (33.13%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,44 +1,46 @@
 class Node:
-    def __init__(self, key, val):
-        self.next = self.prev = None
-        self.val = val
+    def __init__(self, key = 0, val = 0):
         self.key = key
+        self.val = val
+        self.next = self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
-        self.left, self.right = Node(0,0), Node(0,0)
+        self.right, self.left = Node(), Node()
         self.left.next, self.right.prev = self.right, self.left
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
+            self._remove(self.cache[key])
+            self._insert(self.cache[key])
             return self.cache[key].val
         return -1
         
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])
+            self._remove(self.cache[key])
         self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-        
-        if len(self.cache)>self.capacity:
+        self._insert(self.cache[key])
+
+        if len(self.cache) > self.capacity:
             lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
-            
-    def insert(self,node):
-        prv, nxt = self.right.prev, self.right
-        prv.next = nxt.prev = node
-        node.next, node.prev = nxt, prv
-        
-    def remove(self,node):
+            self._remove(lru)
+            del self.cache[lru.key]    
+
+    def _insert(self, node):
+        prv = self.right.prev
+        prv.next = node
+        node.next, node.prev = self.right, prv
+        self.right.prev = node
+
+    def _remove(self, node):
         prv, nxt = node.prev, node.next
         prv.next, nxt.prev = nxt, prv
+
             
         
 
