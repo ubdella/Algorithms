971bc5b9d628fd069286b22ad21035ebf0ba7d32
Abdullah Mohammed <114382137+ubdella@users.noreply.github.com>
2024-10-12 04:43:00 -0400

    Time: 117 ms (73.64%), Space: 17.8 MB (84.91%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,32 +1,26 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
         def dfs(i, j):
-            if i == m or j == n or i < 0 or j < 0 or board[i][j] != "O": 
-                return 
-            board[i][j] = "T"
-            for di, dj in (1, 0), (-1, 0), (0, -1), (0, 1):
+            if i < 0 or j < 0 or i == len(board) or j == len(board[0]) or board[i][j] != 'O':
+                return
+            board[i][j] = 'NIGGA'
+            
+            for di, dj in (0, 1), (1, 0), (0, -1), (-1, 0):
                 dfs(i + di, j + dj)
-        m, n = len(board), len(board[0])
-        for i in range(m):
+                
+        for i in range(len(board)):
             dfs(i, 0)
-            dfs(i, n - 1)
-        
-        for i in range(n):
+            dfs(i, len(board[0]) - 1)
+            
+        for i in range(len(board[0])):
             dfs(0, i)
-            dfs(m -1, i)
-            
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == "T":
-                    board[i][j] = "O"
-                else:
-                    board[i][j] = "X"
-                
-                
+            dfs(len(board) - 1, i)
             
             
-        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                if board[i][j] == 'NIGGA':
+                    board[i][j] = 'O'
         
